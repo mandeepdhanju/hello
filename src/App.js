f8b891c19357a6da51bff0ad4af893a1977244a2
mandeep Dhanju <mdhanju2@my.bcit.ca>
2022-01-19 01:11:04 -0800

    user image upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
+export default function App() {
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    async function getImages() {
+      const result = await axios.get("/api/images")
+      const images = result.data.images
+      setImages(images)
+      console.log(images)
+    }
+    getImages()
+  }, [])
+
+  const submit = async event => {
+    event.preventDefault()
+
+    const formData = new FormData()
+    formData.append("image", file)
+    formData.append("description", description)
+  
+    const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+    console.log(result.data.image)
+    setImages([result.data.image, ...images])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <form onSubmit={submit}>
+        <input
+          filename={file} 
+          onChange={e => setFile(e.target.files[0])} 
+          type="file" 
+          accept="image/*"
+        ></input>
+        <input
+          onChange={e => setDescription(e.target.value)} 
+          type="text"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
 
-export default App;
+      {images.map(image => (
+        <figure key={image.id}>
+          <img src={image.file_path}></img>
+          <figcaption>{image.description}</figcaption>
+        </figure>
+      ))}
+    </div>
+  )
+}
